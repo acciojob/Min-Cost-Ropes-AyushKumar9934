283e9ad6bc204d91f8d1393a966491cce0a63b68
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-23 14:46:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sum=0;
+	arr=arr.sort().reverse();
+	for(let i=0;i<arr.length();i++){
+		sum+=arr[i];
+		for(let j=i+1;j<arr.length();j++){
+			sum+=arr[j];
+		}
+	}
+	
+	return sum;
   
 }
 
