6731f0bf7bea2d30e428203c0a3531c2f28e0cc1
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-23 18:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function mincost(arr)
 //write your code here
 // return the min cost
 	let sum=0;
-	arr=arr.sort().reverse();
-	for(let i=0;i<arr.length();i++){
+	arr=arr.sort((a,b)=>b-a)
+	for(let i=0;i<arr.length;i++){
 		sum+=arr[i];
-		for(let j=i+1;j<arr.length();j++){
+		for(let j=i+1;j<arr.length;j++){
 			sum+=arr[j];
 		}
 	}
 	
-	return sum;
+	return sum-arr[arr.length-1];
   
 }
 
