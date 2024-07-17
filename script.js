9aa03350c1470572858231d600ec88a5a82aa8dd
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 13:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-	let sum=0;
-	arr=arr.sort((a,b)=>a-b)
-	for(let i=0;i<arr.length-1;i++){
-		let connect=arr[i]+arr[i+1];
-		sum+=connect;
-		arr[i+1]=connect;
-	}
+
+{  
+    let sum=0;
+    if(arr.length==1){
+    
+    return arr[0]}
+for(let i=0;i<arr.length-1;i++){
+    arr.sort((a,b)=>a-b);
+    sum+=arr[i]+arr[i+1];
+   
+    arr[i+1]+=arr[i];
+
+}
+return sum;
 	
-	return sum;
   
 }
 
